079c26e66853bc007e4aca3c9b2a4e743689cc17
prajitha18 <prajitha1603@gmail.com>
2026-03-19 23:06:09 +0530

    NGINX improvements, and NFS consistency demonstration

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/metrics", to: proc { [200, {}, ["OK"]] }
   # Defines the root path route ("/")
   root "posts#index"
 end
